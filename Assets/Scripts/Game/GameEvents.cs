511fa5c7fe7a4fe87d0684fc3d2b6fdd22e275f6
Ojas <sharmaojas3@gmail.com>
2022-10-29 01:06:07 +0530

    ReOrdered a few scripts

---
+++ Assets/Scripts/Game/GameEvents.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Game
+{
+    public class GameEvents : MonoBehaviour
+    {
+        public Button btnPause;
+        public GameObject gameOverPanel;
+
+        private void Update()
+        {
+            if (!PlayerManager.isGameStarted)
+                return;
+
+            if (PlayerManager.gameOver)
+            {
+                gameOverPanel.SetActive(true);
+                btnPause.interactable = false;
+            }
+            else
+            {
+                btnPause.interactable = true;
+            }
+        }
+        public void ReplayGame()
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+        public void GoToMenu()
+        {
+            SceneManager.LoadScene("MenuScene");
+        }
+        public void PauseGame()
+        {
+            if (!PlayerManager.isGamePaused && !PlayerManager.gameOver)
+            {
+                Time.timeScale = 0;
+                PlayerManager.isGamePaused = true;
+            }
+        }
+        public void ResumeGame()
+        {
+            if (PlayerManager.isGamePaused)
+            {
+                Time.timeScale = 1;
+                PlayerManager.isGamePaused = false;
+            }
+        }
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
